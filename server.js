1e369dafa0b6ae0e1cf9567979ebdd61dc3bdd24
Gurkan ILLEEZ <gilleez@netas.com.tr>
2020-03-10 12:13:59 +0300

    node js api call finished

--- server.js
+++ server.js
@@ -77,5 +77,155 @@ app.delete('/2fa/deleteCode', function (req, res) {
 		res.send({respone: 'sendCode error'});
 	})
 });
- 
-app.listen(3000)
+
+app.post('/sms/sendMessage',function(req,res) {
+	const message = req.body.message;
+	client.conversation.createMessage({
+      destinationAddress: '+12026212896',
+      senderAddress: '+12026212896',
+      message: message,
+    }).then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+	
+});
+
+app.get('/sms/messages', function(req,res) {
+	client.conversation.getMessages({
+		localAddress: '+12026212896',
+		remoteAddress: '+12026212896'
+	})
+	.then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+	
+});
+
+app.post('/sms/messageStatus', function(req,res) {
+	const messageId = req.body.messageId;
+	client.conversation.getStatus({
+		localAddress: '+12026212896',
+		remoteAddress: '+12026212896',
+		messageId: messageId
+	})
+	.then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+	
+});
+
+app.get('/sms/messagesInThread',function(req,res){
+	client.conversation.getMessagesInThread({
+		localAddress: '+12026212896',
+		remoteAddress: '+12026212896'
+	})
+	.then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+});
+
+
+app.delete('/sms/deleteMessage',function(req,res){
+	const messageId = req.body.messageId;
+	client.conversation.deleteMessage({
+		localAddress: '+12026212896',
+		messageId: messageId,
+		remoteAddress: '+12026212896'
+	})
+	.then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+});
+
+
+app.get('/sms/getSubscriptions',function(req,res){
+	client.conversation.getSubscriptions({})
+	.then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+});
+
+app.post('/sms/getSubscription',function(req,res){
+	const subscriptionId = req.body.subscriptionId;
+	client.conversation.getSubscription({
+		subscriptionId: subscriptionId
+	})
+	.then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+});
+
+app.post('/sms/unsubscribe',function(req,res){
+	const subscriptionId = req.body.subscriptionId;
+	client.conversation.unsubscribe({
+		subscriptionId: subscriptionId
+	})
+	.then(response => {
+		console.log("Response : " + response);
+		res.send(response);
+	}).catch(error => {
+		console.log("ERROR");
+		console.log(error);
+		res.send(error);
+	});
+});
+
+
+
+app.post('/webhook',function(req,res) {
+	console.log("WEB HOOK Triggered");
+	res.send("webhook success");
+})
+
+app.listen(3000);
+
+createInboundSubscription();
+
+async function createInboundSubscription() {
+  try {
+    const response = await client.conversation.subscribe({
+      webhookURL: 'http://10.254.17.244:3000/webhook',
+      destinationAddress: '+12026212896',
+    });
+	console.log("SUCCESS SUBSCRPTION");
+	console.log(response);
+  } catch (error) {
+    console.log("FAIL SUBSCRPTION");
+	console.log(error);
+  }
+}
